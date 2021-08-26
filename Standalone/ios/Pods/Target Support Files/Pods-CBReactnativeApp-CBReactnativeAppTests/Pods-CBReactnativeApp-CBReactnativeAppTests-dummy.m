f5b67fbbcf38a609f6f96de40befbc6194c9dc1c
Linkitsoft <73120628+Linkitsoft@users.noreply.github.com>
2021-08-27 01:40:52 +0500

    Folder Fixature 2.0

--- Standalone/ios/Pods/Target Support Files/Pods-CBReactnativeApp-CBReactnativeAppTests/Pods-CBReactnativeApp-CBReactnativeAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CBReactnativeApp_CBReactnativeAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_CBReactnativeApp_CBReactnativeAppTests
-@end
